753405a85908c471b922ee666789419c2dd603a3
Omar Alvarez <omajul2@hotmail.com>
2016-08-27 10:23:31 +0100

    Include ViewHelpers module in class

--- app.rb
+++ app.rb
@@ -1,10 +1,31 @@
 require 'sinatra/base'
-# require './lib/player'
+require './lib/game'
+require './views/helpers/view_helpers.rb'
 
 class Hangman < Sinatra::Base
 
+  include ViewHelpers
+
+  enable :sessions
+
+  # This is a sinatra filter (@game is used in every route)
+  before do
+    @game = Game.instance
+  end
+
   get '/' do
-    "Hello World"
+    erb :start_game
+  end
+
+  post '/game_data' do
+    word = params[:word]
+    clue = params[:clue]
+    @game = Game.create(word, word.length, clue)
+    redirect '/play'
+  end
+
+  get '/play' do
+    erb :play
   end
 
   # start the server if ruby file executed directly
